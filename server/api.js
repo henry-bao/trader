139a6431eed02f7c891da631aefac6f73569e86a
henry-bao <henrybao@uw.edu>
2023-05-14 00:28:23 -0700

    updated readme

--- server/api.js
+++ server/api.js
@@ -18,6 +18,7 @@ const upload = multer({
 module.exports = function (app) {
   const minioClient = new Client({
     endPoint: process.env.MINIO_ENDPOINT,
+    port: parseInt(process.env.MINIO_PORT),
     useSSL: true,
     accessKey: process.env.MINIO_ACCESS_KEY,
     secretKey: process.env.MINIO_SECRET_KEY,
